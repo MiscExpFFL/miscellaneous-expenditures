d936e440e1f48b03dd9d53ee59112ad73d2861bb
MiscExpFFL <tomtom2023@yahoo.com>
2026-09-23 23:24:01 -0700

    Advance weekly sync to Week 3 Wednesday layers

--- me-weekly-sync.js
+++ me-weekly-sync.js
@@ -1,1 +1,5 @@
 document.write('<script src="week2-wednesday-data.js?v=meffl-20260916-wed1"></script>');
+document.write('<script src="week3-yahoo-data.js?v=meffl-20260923-wed1"></script>');
+document.write('<script src="week2-results-close.js?v=meffl-20260923-close1"></script>');
+document.write('<script src="week3-models.js?v=meffl-20260923-model1"></script>');
+document.write('<script src="week3-wednesday-editorial.js?v=meffl-20260923-edit1"></script>');
