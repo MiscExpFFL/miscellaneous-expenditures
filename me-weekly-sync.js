d3f424d397ef8b47af8e3bb5a08c87fb241c671c
MiscExpFFL <tomtom2023@yahoo.com>
2026-09-23 23:30:28 -0700

    Load reusable Wednesday site refresh

--- me-weekly-sync.js
+++ me-weekly-sync.js
@@ -3,3 +3,4 @@ document.write('<script src="week3-yahoo-data.js?v=meffl-20260923-wed1"></script
 document.write('<script src="week2-results-close.js?v=meffl-20260923-close1"></script>');
 document.write('<script src="week3-models.js?v=meffl-20260923-model1"></script>');
 document.write('<script src="week3-wednesday-editorial.js?v=meffl-20260923-edit1"></script>');
+document.write('<script src="wednesday-site-refresh.js?v=meffl-20260923-site1"></script>');
